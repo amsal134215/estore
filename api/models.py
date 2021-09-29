27f261d9e5d3ae0054472d1c3d412776710aa215
Muhammad Amsal <amsal134215@gmail.com>
2021-09-29 06:26:49 +0500

    model updates.

--- api/models.py
+++ api/models.py
@@ -21,6 +21,6 @@ class ProductCoupon(models.Model):
 
 class CustomerSubscription(models.Model):
     subscription_code = models.CharField(max_length=100, unique=True)
-    products = models.ManyToManyField(ProductCoupon)
+    products = models.ManyToManyField(ProductCoupon, null=True, blank=True)
     global_disc_percent = models.FloatField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
